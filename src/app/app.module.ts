8784b6a1f9f86be04145b08f5f49d07e2031ff3c
Clairton Lima <108296651+Clairtonlm@users.noreply.github.com>
2025-02-26 17:00:33 -0300

    refactor: update package.json dependencies and scripts; add HomeComponent with basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/wheater/page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,//importamos essa
+    HttpClientModule,//essa
+    FormsModule,//essa
+    //FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
