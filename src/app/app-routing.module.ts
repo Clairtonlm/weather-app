1e8121b1670d1414e2ac9467a862c14904f97920
Clairton Lima <108296651+Clairtonlm@users.noreply.github.com>
2025-02-26 17:13:53 -0300

    Crinado primeira rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/wheater/page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',//quando acessamos o caminho estar vazio
+    redirectTo: 'weather',//vá para weather
+    pathMatch:'full'
+  },
+  //cria a rota weather
+  {
+    path:'weather',//nome do project
+    component:HomeComponent,//componet exibirdo
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
