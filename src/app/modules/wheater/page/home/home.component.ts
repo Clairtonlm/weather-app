5f00aedefb25d8c88c0b48a1537ddd9cd65bea97
Clairton Lima <108296651+Clairtonlm@users.noreply.github.com>
2025-02-27 10:27:00 -0300

    feat: implement weather service and data interface; add HomeComponent with weather data fetching

--- src/app/modules/wheater/page/home/home.component.ts
+++ src/app/modules/wheater/page/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherServices } from '../../servicess/weather.services';
+import { weatherDatas } from 'src/app/models/interfaces/weatherData';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  //temos que iniciar uma cidade
 
+  initialCityname = 'Fortaleza';
+   //criar uma propriedade para amarzenar os dados que queremos
+  weatherDatas!: weatherDatas;
+ 
+//criar um constructor
+  constructor(private weatherService: WeatherServices){}
+  ngOnInit(): void {
+   this.getEheatherDatas(this.initialCityname);// inicia com fortaleza como cidade padrão.
+  }
+
+  //vamos criar um metodo para consumir esse serviço
+  getEheatherDatas(cityName: string): void{
+    this.weatherService.getWeatherDatas(cityName)
+    //temos que no increver nele
+    .subscribe({
+      next:(response)=>{
+        response && (this.weatherDatas = response);
+        console.log(this.weatherDatas)
+      },
+      error: (error)=> console.log(error)
+    });
+  }
 }
--- src/app/modules/wheater/servicess/weather.services.ts
+++ src/app/modules/wheater/servicess/weather.services.ts
@@ -1,10 +1,19 @@
+//metodo de serviço
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class WeatherServices{
-    constructor(){
+    private apiKey = 'f32051ab9f796164481f8fcd930a7bcd';//apikey openMap
+    constructor(private http: HttpClient){
+
+    }
+    getWeatherDatas(cityName: string): Observable<any>{
+        return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`, {})
+       
+        }
         
     }
-}
